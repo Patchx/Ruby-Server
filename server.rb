0637ae3406150cc6a75fadbfe71e0e349ce657fa
Patchx <robert.anderson.fl@gmail.com>
2016-02-09 18:12:19 -0500

    changed server to input any stock id

--- server.rb
+++ server.rb
@@ -3,12 +3,18 @@ require 'httparty'
 require 'nokogiri'
 require 'active_support/all'
 
-get '/' do
-	response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+get '/?' do
+	response = HTTParty.get("http://finance.yahoo.com/q?s=#{params[:stock]}")
 	dom = Nokogiri::HTML(response.body)
-	ticker_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+	if params[:stock] == nil
+		return "Welcome to the stock ticker scrapper. Please enter the stock data into your brower in the following format:  localhost:4567/?stock=AAPL"
+	end
+	ticker_span = dom.xpath("//span[@id='yfs_l84_#{params[:stock].downcase}']")
+	if ticker_span.children[0] == nil
+		return "Sorry, I don't have a record of that stock"
+	end
 	price = ticker_span.children[0].content.to_f.to_s(:currency)
-	return "Apple's current stock price is #{price}"
+	return "Current stock price for #{params[:stock]} is #{price}"
 end
 
 get '/sinatra' do
