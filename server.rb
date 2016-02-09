6246fafa47f6acff27b0512ff3748fbf5b91a8e9
Patchx <robert.anderson.fl@gmail.com>
2016-02-09 17:28:51 -0500

    migrated the stock scrapper functions to the server.rb file

--- server.rb
+++ server.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
+require 'active_support/all'
 
 get '/' do
-	"Hello world"
+	response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+	dom = Nokogiri::HTML(response.body)
+	ticker_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+	price = ticker_span.children[0].content.to_f.to_s(:currency)
+	return "Apple's current stock price is #{price}"
 end
 
 get '/sinatra' do
--- stock_scrapper.rb
+++ stock_scrapper.rb
@@ -2,11 +2,13 @@ require 'httparty'
 require 'nokogiri'
 require 'active_support/all'
 
-response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
+module Display_Stock_Price
+	response = HTTParty.get('http://finance.yahoo.com/q?s=AAPL')
 
-dom = Nokogiri::HTML(response.body)
+	dom = Nokogiri::HTML(response.body)
 
-ticker_span = dom.xpath("//span[@id='yfs_l84_aapl']")
+	ticker_span = dom.xpath("//span[@id='yfs_l84_aapl']")
 
-price = ticker_span.children[0].content.to_f.to_s(:currency)
-puts "Apple's current stock price is #{price}"
+	price = ticker_span.children[0].content.to_f.to_s(:currency)
+	"Apple's current stock price is #{price}"
+end
